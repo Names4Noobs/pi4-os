df0908585f4e3e77193964e6749abdfc6d36088b
Names4Noobs <62076851+Names4Noobs@users.noreply.github.com>
2021-10-30 15:56:57 -0400

    RPI 4 framebuffer stuff

--- run.sh
+++ run.sh
@@ -1,4 +1,26 @@
-command echo "$AARCH64_QEMU_BIN"
-"$AARCH64_QEMU_BIN" \
-		-machine raspi3 \
-		-kernel kernel8.img
+#!/bin/sh
+
+CMD=$1
+
+#"$AARCH64_QEMU_BIN" \
+#		-machine raspi3 \
+#		-serial stdio   \
+#		-kernel kernel8.img
+
+run_qemu() {
+ "$AARCH64_QEMU_BIN" \
+     -M raspi3 \
+     -serial stdio   \
+     -kernel kernel8.img
+}
+
+if [ "$CMD" = "raspi3" ]
+then
+		echo "Running raspi3 machine"
+		run_qemu
+		
+else
+
+	echo "Entered $CMD"
+ 
+fi
